81ba044d37967b54075adf0e6b02b5a175547a9c
Jonas Schumacher <jonas@Jonass-iMac.fritz.box>
2016-10-16 01:07:34 +0200

    Initial commit. Basic scraper, first test on morph.io

--- scraper.py
+++ scraper.py
@@ -22,3 +22,39 @@
 # All that matters is that your final data is written to an SQLite database
 # called "data.sqlite" in the current working directory which has at least a table
 # called "data".
+
+import re
+import pickle
+import datetime
+
+from bs4 import BeautifulSoup
+import scraperwiki
+
+dates = {
+    0: datetime.date.today(),
+    1: datetime.date.today() + datetime.timedelta(1),
+    2: datetime.date.today() + datetime.timedelta(2),
+    3: datetime.date.today() + datetime.timedelta(3),
+}
+
+with open("cities.txt", "rb") as f:
+    cities = pickle.load(f)
+    for city in cities[:3]:
+        for i in range(4):
+            replaced_city = city.encode("utf-8").replace(" ", "+")
+            url = "http://www.google.com/movies?near={}&date={}".format(replaced_city, i)
+            page = scraperwiki.scrape(url)
+            soup = BeautifulSoup(page, "html.parser")
+            movies = soup.find_all(string=re.compile(".*\(OV\).*"))
+            for movie in movies:
+                complete_info = movie.find_parent('div', class_="movie")
+                theater = movie.find_parent("div", class_="theater")
+                data = {
+                    "stadt": city,
+                    "kino": theater.find("h2", class_="name").string,
+                    "film": complete_info.find("div", class_="name").string,
+                    "tag": dates[i],
+                    "zeiten": ",".join([child.contents[2] for child in complete_info.find("div", class_="times").children])
+                }
+                # print data
+                scraperwiki.sqlite.save(unique_keys=["stadt", "kino", "film", "tag"], data=data)
