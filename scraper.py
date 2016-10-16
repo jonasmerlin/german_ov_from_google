09dbfb924bc59e59264f230d3de50c4baa04b158
Jonas Schumacher <jonas@Jonass-iMac.fritz.box>
2016-10-16 17:28:36 +0200

    OmUs are now searched for as well. OV Type is saved as a seperate attribute

--- scraper.py
+++ scraper.py
@@ -23,6 +23,8 @@
 # called "data.sqlite" in the current working directory which has at least a table
 # called "data".
 
+# TODO: look if encoding can be set at save
+
 import re
 import pickle
 import datetime
@@ -40,20 +42,25 @@ dates = {
 
 with open("cities.txt", "rb") as f:
     cities = pickle.load(f)
-    for city in cities:
+    for city in cities[:3]:
         for i in range(4):
             replaced_city = city.encode("utf-8").replace(" ", "+")
             url = "http://www.google.com/movies?near={}&date={}".format(replaced_city, i)
             page = requests.get(url)
             soup = BeautifulSoup(page.text, "html.parser")
-            movies = soup.find_all(string=re.compile(".*\(OV\).*"))
+            movies = soup.find_all(string=re.compile(".*\(OV\).*|.*\(OmU\).*"))
             for movie in movies:
                 complete_info = movie.find_parent('div', class_="movie")
                 theater = movie.find_parent("div", class_="theater")
+                film_and_type = unicode(complete_info.find("div", class_="name").string)
+                film_and_type_re = re.match("(.*) \((OV|OmU)\)", film_and_type)
+                film = film_and_type_re.group(1)
+                ov_type = film_and_type_re.group(2)
                 data = {
                     "stadt": unicode(city),
                     "kino": unicode(theater.find("h2", class_="name").string),
-                    "film": unicode(complete_info.find("div", class_="name").string),
+                    "film": film,
+                    "typ": ov_type,
                     "tag": dates[i],
                     "zeiten": ",".join([child.contents[2] for child in complete_info.find("div", class_="times").children])
                 }
